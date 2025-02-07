76023a18d5d41a15bea0daf4bd2a8c61ada43be9
adamar <aysun114@hotmail.com>
2025-02-07 13:38:07 -0600

    ToolTipTest added

---
+++ src/test/java/cydeoInterview/tests/ToolTipTest.java
@@ -0,0 +1,23 @@
+package cydeoInterview.tests;
+
+import cydeoInterview.utilities.ConfigurationReader;
+import cydeoInterview.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ToolTipTest {
+
+    @Test
+    public void tool_tip_test(){
+
+        //     1. Go to https://practice.expandtesting.com/tooltips
+        Driver.getDriver().get(ConfigurationReader.getProperty("toolTip.test.url"));
+
+        //     2. Verify tooltip for the element "Tooltip on bottom" is displayed correctly.
+        WebElement toolTip = Driver.getDriver().findElement(By.id("btn3"));
+
+        Assert.assertEquals(toolTip.getAttribute("title"),"Tooltip on bottom");
+    }
+}
